bf60afe090e3a22005380bd14e1ee670c98850bf
Junah Park <junahp@hotmail.com>
2020-10-20 19:25:01 -0700

    heroku deployment configurations

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 let cors = require('cors');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const drinks = require("./routes/api/drinks");
-
 
 const app = express();
 
@@ -41,6 +41,16 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/drinks", drinks);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 // CORS
 // app.use((req, res, next) => {
